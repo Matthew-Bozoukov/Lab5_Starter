a716815f56dacfc4d54b9fd27ab7b09cbccc7acc
Matthew-Bozoukov <matthewbozoukov@gmail.com>
2026-05-04 09:47:53 -0700

    added first version of explore

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Lab 5 - Starter
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
-[expose](https://Matthew-Bozoukov.github.io/Lab5_Starter/expose.html)
+[expose](https://Matthew-Bozoukov.github.io/Lab5_Starter/expose.html) [explore](https://Matthew-Bozoukov.github.io/Lab5_Starter/explore.html)
 Matthew Bozoukov
 Team members: Matthew Bozoukov
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,72 @@
-// explore.js
-
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  // TODO
+  const faceImage = document.querySelector("#explore img");
+  const textArea = document.getElementById("text-to-speak");
+  const voiceSelect = document.getElementById("voice-select");
+  const talkButton = document.querySelector("#explore button");
+
+  const smilingFace = "assets/images/smiling.png";
+
+  
+  const openMouthFace = "assets/images/smiling-open.png";
+
+  let voices = [];
+
+  function loadVoices() {
+    voices = window.speechSynthesis.getVoices();
+
+    voiceSelect.innerHTML = "";
+
+    const defaultOption = document.createElement("option");
+    defaultOption.value = "select";
+    defaultOption.textContent = "Select Voice:";
+    defaultOption.disabled = true;
+    defaultOption.selected = true;
+    voiceSelect.appendChild(defaultOption);
+
+    voices.forEach((voice, index) => {
+      const option = document.createElement("option");
+      option.value = index;
+      option.textContent = `${voice.name} (${voice.lang})`;
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  loadVoices();
+
+  window.speechSynthesis.addEventListener("voiceschanged", loadVoices);
+
+  talkButton.addEventListener("click", function () {
+    const text = textArea.value;
+
+    if (text.trim() === "") {
+      return;
+    }
+
+    window.speechSynthesis.cancel();
+
+    const utterance = new SpeechSynthesisUtterance(text);
+
+    if (voiceSelect.value !== "select") {
+      utterance.voice = voices[Number(voiceSelect.value)];
+    }
+
+    utterance.onstart = function () {
+      faceImage.src = openMouthFace;
+      faceImage.alt = "Open mouth face";
+    };
+
+    utterance.onend = function () {
+      faceImage.src = smilingFace;
+      faceImage.alt = "Smiling face";
+    };
+
+    utterance.onerror = function () {
+      faceImage.src = smilingFace;
+      faceImage.alt = "Smiling face";
+    };
+
+    window.speechSynthesis.speak(utterance);
+  });
 }
