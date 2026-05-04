27e350621c648410605894ee06a5de55ecba01f1
Matthew-Bozoukov <matthewbozoukov@gmail.com>
2026-05-04 10:49:44 -0700

    commited unit_test.js and readme

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Make sure you make a PR to your own repo's main and not the class' repo!! Otherw
 [expose](https://Matthew-Bozoukov.github.io/Lab5_Starter/expose.html) [explore](https://Matthew-Bozoukov.github.io/Lab5_Starter/explore.html)
 Matthew Bozoukov
 Team members: Matthew Bozoukov
+
+# Question 1
+I would not use only a unit test to test the full “message” feature, because sending a message involves multiple parts at work. Unit testing is specialized for testing individual components
+# Question 2
+Yes, I would use a unit test for the “max message length” feature because it is an isolated behavior. The test could check that messages with 80 characters or fewer are allowed, while messages longer than 80 characters are rejected or prevented.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,94 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+import * as functions from "../code-to-unit-test/unit-test-me.js";
+
+describe("isPhoneNumber", () => {
+  test("returns true for valid phone number", () => {
+    expect(functions.isPhoneNumber("123-456-7890")).toBe(true);
+  });
+
+  test("returns true for valid phone number but with paranthesis", () => {
+    expect(functions.isPhoneNumber("(123) 456-7890")).toBe(true);
+  });
+
+  test("returns false for phone number with no dashes", () => {
+    expect(functions.isPhoneNumber("1234567890")).toBe(false);
+  });
+
+  test("returns false for non-phone-number text", () => {
+    expect(functions.isPhoneNumber("hello")).toBe(false);
+  });
+});
+
+describe("isEmail", () => {
+  test("returns true for a valid email", () => {
+    expect(functions.isEmail("student@example.com")).toBe(true);
+  });
+
+  test("returns true for a valid email with underscore", () => {
+    expect(functions.isEmail("student_name@example.edu")).toBe(true);
+  });
+
+  test("returns false for email without @ symbol", () => {
+    expect(functions.isEmail("student.example.com")).toBe(false);
+  });
+
+  test("returns false for email without a valid domain extension", () => {
+    expect(functions.isEmail("student@example")).toBe(false);
+  });
+});
+
+describe("isStrongPassword", () => {
+  test("returns true for valid password starting with a letter", () => {
+    expect(functions.isStrongPassword("abcd")).toBe(true);
+  });
+
+  test("returns true for vaLID password with letters, numbers, and underscore", () => {
+    expect(functions.isStrongPassword("A123_456")).toBe(true);
+  });
+
+  test("returns false for password starting with a number", () => {
+    expect(functions.isStrongPassword("1abc")).toBe(false);
+  });
+
+  test("returns false for password with invalid character", () => {
+    expect(functions.isStrongPassword("abc!")).toBe(false);
+  });
+});
+
+describe("isDate", () => {
+  test("returns true for valid date", () => {
+    expect(functions.isDate("1/2/2026")).toBe(true);
+  });
+
+  test("returns true for valid date with two digit month and day", () => {
+    expect(functions.isDate("12/31/2026")).toBe(true);
+  });
+
+  test("returns false for date with dashes", () => {
+    expect(functions.isDate("12-31-2026")).toBe(false);
+  });
+
+  test("returns false for date with two digit year", () => {
+    expect(functions.isDate("12/31/26")).toBe(false);
+  });
+});
+
+describe("isHexColor", () => {
+  test("returns true for valid 3 character hex color with #", () => {
+    expect(functions.isHexColor("#fff")).toBe(true);
+  });
+
+  test("returns true for valid 6 character hex color", () => {
+    expect(functions.isHexColor("123abc")).toBe(true);
+  });
+
+  test("returns false for hex color with too few characters", () => {
+    expect(functions.isHexColor("#12")).toBe(false);
+  });
+
+  test("returns false for hex color with invalid characters", () => {
+    expect(functions.isHexColor("#ggg")).toBe(false);
+  });
+});
